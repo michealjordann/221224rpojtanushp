e1aaedfca641e98e5433588068b9e88c6817d362
Tanush P <148394954+michealjordann@users.noreply.github.com>
2026-01-16 14:21:39 -0800

    Update Player.java

--- CSA5/Player.java
+++ CSA5/Player.java
@@ -1,49 +1,51 @@
 /*
  * Activity 2.2.2
- *
+ * 
  * A Player class the PhraseSolverGame
  */
 import java.util.Scanner;
-
 
 public class Player
 {
   /* your code here - attributes */
   private String name;
   private int points;
-
-
-  /* your code here - constructor(s) */
+  /* your code here - constructor(s) */ 
+  public String getName()
+ {
+   return name;
+ }
+ 
+ public void setName(String x)
+ {
+   name = x;
+ }
+  
+ public double getPoints()
+ {
+   return points;
+ }
+ 
+ public void addPoints(int newPoints){
+   points = points + newPoints;
+ }
+ 
   public Player(){
-    System.out.println("Welcome, Enter your name:");
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter Your Name");
-    name = sc.nextLine();
-    points = 0;
-    System.out.print("Welcome" + name);
+  
+  Scanner sc = new Scanner(System.in);
 
+  System.out.println("Enter Your Name");
+  name = sc.nextLine();
+  points = 0;
+  System.out.println("Welcome " + name + "!");
 
   }
-
 
   public Player(String inputName){
+  
+  name = inputName;
+  points = 0;
+  System.out.println("Welcome " + name + "!");
 
-
-    name = inputName;
-    points = 0;
-    System.out.print("Welcome" + name);
   }
- 
-
-
-  /* your code here - accessor(s) */
-public String getName(){
-  return name;
-}
-public int points(){
-  return points;
-}
-
-
-  /* your code here - mutator(s) */
 }
